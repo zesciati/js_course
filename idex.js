95cf64915aa7bdcaa6f20a2bf087e5268c3b4732
zesciati <zakyabiyyu2003@gmail.com>
2024-11-22 23:59:44 +0800

    belajar boolean dan if statement

--- idex.js
+++ idex.js
@@ -1,10 +1,23 @@
+let myVariable = 6;
+let myVariable2 = "Hello";
+console.log("🚀 ~ file: idex.js:4 ~ myVariable2:", myVariable2);
 
+let myVariable3 = true;
 
+console.log("🚀 ~ file: idex.js:5 ~ myVariable3:", myVariable3);
+console.log("🚀 ~ file: idex.js:5 ~ myVariable:", myVariable);
 
-  let myVariable = 6;
-  console.log("🚀 ~ file: idex.js:5 ~ myVariable:", myVariable);
-  
-  
+// ------------------------------------------------------------------------
 
+function ninja(a, b) {
+  return a + b;
+}
+
+const c = ninja("zaky ", "abiyyu");
+console.log("🚀 ~ file: idex.js:20 ~ c:", c);
+
+const bravo = '5';
+const delta = 5 === bravo;
+console.log("🚀 ~ file: idex.js:21 ~ delta:", delta);
 
 
